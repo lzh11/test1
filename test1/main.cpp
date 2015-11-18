9377b8c62b884f8ebe2501f7a3f63582eb4cf9b1
lzh11 <2239040999@qq.com>
2015-11-18 19:35:50 +0800

    test1
    第二次提交

--- test1/main.cpp
+++ test1/main.cpp
@@ -10,6 +10,10 @@
 
 int main(int argc, const char * argv[]) {
     // insert code here...
-    std::cout << "Hello, World!\n";
+    int a,*p;
+    a=100;
+    p=&a;
+    printf("a=%x &a=%x p=%x &p=%x *p=%x\n",a,&a,p,&p,*p);
+    //std::cout << "Hello, World!\n";
     return 0;
 }
